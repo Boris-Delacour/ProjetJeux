ac252f4f041da0406e5a0f0d56fc7b98f9abd9fd
Boris Delacour <boris.delacour@outlook.fr>
2018-06-27 14:17:41 +0200

    Added image

--- src/ihm/MainFrame.java
+++ src/ihm/MainFrame.java
@@ -1,11 +1,35 @@
 package ihm;
 
 import javafx.application.*;
+import javafx.geometry.*;
+import javafx.scene.*;
+import javafx.scene.image.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.*;
 import javafx.stage.*;
 
 public class MainFrame extends Application {
 	@Override
-	public void start(Stage arg0) throws Exception {
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Jeux");
+		primaryStage.setWidth(1600);
+		primaryStage.setHeight(960);
 		
+		BorderPane pane = new BorderPane();
+		pane.setMinWidth(1600);
+		pane.setMinHeight(960);
+		
+		pane.setBackground(new Background(new BackgroundFill(Color.web("#000000"), CornerRadii.EMPTY, Insets.EMPTY)));
+		
+		ImageView i = new ImageView(new Image("ressources/Tamriel.png"));
+		pane.setCenter(i);
+						
+		primaryStage.setScene(new Scene(pane));
+		primaryStage.sizeToScene();
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
 	}
 }
